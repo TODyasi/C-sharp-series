ef1a6d0d94fb880278767fda30c6aa236dff8ccc
OkuhleDyasi <thandokuhle.dyasi@gmail.com>
2025-05-26 10:17:11 +0000

    Add project files.

---
+++ C# series/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace C__series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string getGreeting = Greeting().ToString();
+            Console.WriteLine(getGreeting);
+        }
+
+        static string Greeting()
+        {
+            string greeting = "Wagwan my people in the physical world \nMy name is";
+            string name = "AuxDev";
+            string from = "\nfrom the bombaclat country of South Africa";
+
+            return greeting + name + from;
+        }
+    }
+}
