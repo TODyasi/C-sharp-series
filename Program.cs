dbd26be4c4c1fa538ed0e3906f74d33073b998d5
Okuhle dyasi <166889804+TODyasi@users.noreply.github.com>
2025-05-26 10:31:34 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace C__series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting();
+        }
+
+        static void Greeting()
+        {
+<<<<<<< Updated upstream
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("What would you like to say to the people");
+            string greeting = Console.ReadLine();
+            Console.WriteLine(name + " " + greeting);
+
+=======
+            Console.WriteLine("Enter your name:");  
+            string name = Console.ReadLine();
+            Console.WriteLine("What do you want to tell the people");
+            string greeting = Console.ReadLine();   
+            Console.WriteLine(name + " " + greeting);
+>>>>>>> Stashed changes
+        }
+    }
+}
